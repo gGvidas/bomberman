501bed452f1a1b0eb4bed4662a44274390000aa3
unknown <gd.gvidas@gmail.com>
2020-09-20 15:17:58 +0300

    Initial commit

---
+++ Bomberman/BombermanHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Bomberman
+{
+    public class BombermanHub : Hub
+    {
+        public void UpdateClients()
+        {
+             Clients.All.SendAsync("StateUpdate", "Hi");
+        }
+    }
+}
---
+++ BombermanClient/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace BombermanClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, System.EventArgs e)
+        {
+            var hubConnection = new HubConnectionBuilder().WithUrl("https://localhost:44344/hub/").Build();
+            hubConnection.On<string>("StateUpdate", message => Label1.Text = message);
+            await hubConnection.StartAsync();
+            await hubConnection.InvokeAsync("UpdateClients");
+        }
+    }
+}
