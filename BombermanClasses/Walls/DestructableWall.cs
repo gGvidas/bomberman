77777df1e158c18affd1893a4d8b8aa387eb1127
Justinas <justinas97@gmail.com>
2020-12-06 13:17:49 +0200

    add get image method to wall

--- BombermanClasses/Walls/DestructableWall.cs
+++ BombermanClasses/Walls/DestructableWall.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Text;
 
 namespace BombermanClasses.Walls
@@ -7,6 +9,15 @@ namespace BombermanClasses.Walls
     [Serializable]
     public class DestructableWall : Wall
     {
+
+        public override Image GetImage()
+        {
+            try
+            {
+                return Image.FromFile(Path.Combine(Directory.GetCurrentDirectory())); 
+            }catch(Exception e) { return null; }
+        }
+
         public override int getScore()
         {
             return 50;
--- BombermanClasses/Walls/IceWall.cs
+++ BombermanClasses/Walls/IceWall.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 
 namespace BombermanClasses.Walls
 {
@@ -6,6 +8,16 @@ namespace BombermanClasses.Walls
     public class IceWall : Wall
     {
         public bool DamageTaken { get; set; } = false;
+
+
+        public override Image GetImage()
+        {
+            try
+            {
+                return Image.FromFile(Path.Combine(Path.Combine(Path.Combine(Directory.GetCurrentDirectory(), @"Sprites\BlockIce.png"))));
+            }
+            catch (Exception e) { return null; }
+        }
 
         protected sealed override bool canDestroy()
         {
